3fea1803c695bedd559aa864db8a74d9fdd934c1
Aengus Stewart <aengus.stewart@crick.ac.uk>
2021-04-01 17:23:47 +0100

    Add Files

---
+++ makeCacheMatrix.R
@@ -0,0 +1,13 @@
+acheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
